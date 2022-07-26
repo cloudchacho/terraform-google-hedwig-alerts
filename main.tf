daa8067866cf0d993a7a1e6b1da15f74a8aa34d4
Aniruddha Maru <aniruddhamaru@gmail.com>
2022-07-26 16:06:24 -0700

    Remove no activity alert

--- main.tf
+++ main.tf
@@ -39,38 +39,6 @@ resource "google_monitoring_alert_policy" "high_message_alert" {
   user_labels = var.labels
 }
 
-resource "google_monitoring_alert_policy" "no_activity_alert" {
-  project = var.alerting_project
-
-  display_name = "${title(var.queue)}'s Hedwig queue has no activity${local.title_suffix}"
-  combiner     = "OR"
-
-  conditions {
-    display_name = "${title(var.queue)}'s Hedwig queue has no activity${local.title_suffix}"
-
-    condition_absent {
-      duration = "300s" // Seconds
-
-      filter = "metric.type=\"pubsub.googleapis.com/subscription/streaming_pull_message_operation_count\" resource.type=\"pubsub_subscription\" (resource.label.\"subscription_id\"=monitoring.regex.full_match(\"hedwig-${var.queue}-.*\") AND resource.labels.\"subscription_id\"!=\"hedwig-${var.queue}-dlq\")${local.filter_suffix}"
-
-      aggregations {
-        alignment_period = "60s"
-        # can't include this because Terraform tries to always apply this change
-        # cross_series_reducer = "REDUCE_NONE"
-        per_series_aligner = "ALIGN_MEAN"
-      }
-
-      trigger {
-        count = 1
-      }
-    }
-  }
-
-  notification_channels = var.queue_no_activity_notification_channels
-
-  user_labels = var.labels
-}
-
 resource "google_monitoring_alert_policy" "dlq_alert" {
   project = var.alerting_project
 
